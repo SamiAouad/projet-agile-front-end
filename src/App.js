9928afb4c663386f69f423ebabed7e99d534b347
SamiAouad <samiaouad7@gmail.com>
2021-12-28 23:58:15 +0000

    create groupe page
    create groupe page initiale

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp'
 import Home from './components/Home';
+import CreateGroupe from './components/CreateGroupe';
 import './App.css';
 
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} exact/>
           <Route path='/signIn' element={<SignIn/>} exact/>
+          <Route path='/createGroupe' element={<CreateGroupe/>} exact/>
           <Route path='/signUp' element={<SignUp/>} exact/>
         </Routes>
       </BrowserRouter>
